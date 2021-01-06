01fb205c8f40c6d054a3599e97429db29b82c81c
苏震 <suzhen@viewhigh.com>
2021-01-06 09:51:42 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
 import { router } from './router'
-import './styles/primer.css'
+// import './styles/primer.css'
 
 const app = createApp(App)
 
